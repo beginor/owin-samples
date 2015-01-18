81e4d59b4017f9d1da38514557824466a71fc78c
beginor <beginor@hotmail.com.com>
2015-01-17 22:32:45 -0800

    return request scope values

--- Owin04_OAuthResource/Controllers/UserController.cs
+++ Owin04_OAuthResource/Controllers/UserController.cs
@@ -1,12 +1,20 @@
-﻿using System.Web.Http;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web.Http;
 
 namespace Owin04_OAuthResource.Controllers {
 
     [Authorize]
     public class UserController : ApiController {
 
-        public string Get() {
-            return User.Identity.Name;
+        public object Get() {
+            var identity = User.Identity as ClaimsIdentity;
+            var infos = identity.Claims.Where(claim => claim.Type == "urn:oauth:scope")
+                .Select(claim => claim.Value)
+                .ToDictionary(s => s, s => s + " value is xxx.");
+
+            return new { name = identity.Name, infos };
         }
     }
 }
